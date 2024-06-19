c21ce81bbd6861ccb9e26a2769129fa10a2a6258
Lam Toan <lamtoan2104@gmail.com>
2024-06-19 15:10:04 +0700

    server

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(cookieParser())
 app.use(cors(
     {
-        origin: ["http://localhost:3000", "https://vanytuong.vercel.app"],
+        origin: ["http://localhost:3000", "https://vanytuong.com"],
         // credentials: true,
         methods:["GET", "POST", "PUT", "DELETE", "PATCH"]
     }
